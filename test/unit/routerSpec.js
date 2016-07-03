55396f9cc4da756e9b7487613413ce7741f86452
Eliran Ben-Ezra <eliran@threeplay.com>
2016-07-03 16:58:33 -0400

    protected routes with jwt state injection to request

--- test/unit/routerSpec.js
+++ test/unit/routerSpec.js
@@ -2,7 +2,9 @@ var helpers = require('../helpers')
   , expect = helpers.expect
   , Router = helpers.Router
   , Promise = helpers.Promise
+  , SessionController = helpers.SessionController
   , MockHttpProvider = require('./mockHttpProvider')
+  , Response = helpers.Response
 
 function mockResponse(){
   return {
@@ -13,13 +15,78 @@ function mockResponse(){
 describe('REST API Router', function(){
 
   it('throws if no httpProvider is supplied', function(){
-     expect(function(){ new Router() }).to.throw
+     expect(function(){ new Router() }).to.throw()
   })
 
   describe('router', function(){
     beforeEach(function(){
       this.mockHttpProvider = new MockHttpProvider()
       this.router = new Router(this.mockHttpProvider) 
+    })
+
+    describe('session & security', function(){
+      beforeEach(function(){
+        this.sessionController = new SessionController('secret')
+        this.router.setSessionController(this.sessionController)
+      })
+
+      it('can have a session controller', function(){
+        expect(this.router.sessionController()).to.equal(this.sessionController)
+      })
+
+      it('#protectRoute throws if no such route', function(){
+        expect(function(){ this.router.protectRoute('get', '/someRoute') }).to.throw()
+      })
+
+      it('a protected route with no token header is returns \'unauthorized\'', function(){
+        var invoker = this.router.addRoute('get', '/path', this, 'onlyIfLoggedIn')
+        this.router.protectRoute('get', '/path')
+        return expect(invoke(invoker, {})).to.become(Response.unauthorized())
+      })
+
+      describe('when session exists', function(){
+        beforeEach(function(){
+          var context = this
+          this.sessionState = { value: 'xx' }
+          return this.sessionController.create(this.sessionState).then(function(token){
+            context.sessionToken = token
+          })
+        })
+
+        it('returns unauthorized if Session controller is invalid', function(){
+          var invoker = this.router.addRoute('get', '/path', this, 'onlyIfLoggedIn')
+          this.router.protectRoute('get', '/path')
+          this.router.setSessionController(null)
+          return expect(invoke(invoker, { 'session-token': this.sessionToken })).to.become(Response.unauthorized())
+        })
+
+        it('returns unauthorized if token is invalid', function(){
+          var invoker = this.router.addRoute('get', '/path', this, 'onlyIfLoggedIn')
+          this.router.protectRoute('get', '/path')
+          return expect(invoke(invoker, { 'session-token': 'abc' })).to.become(Response.unauthorized())
+        })
+
+        it('allows access to protected resource when a valid token is in header ', function(){
+          var invoker = this.router.addRoute('get', '/path', this, 'onlyIfLoggedIn')
+          this.router.protectRoute('get', '/path')
+          this.onlyIfLoggedIn = function(){
+            return Response.ok('logged-in')
+          }
+          return expect(invoke(invoker, { 'session-token': this.sessionToken })).to.become(Response.ok('logged-in'))
+        })
+
+        it('passes session data in request to method', function(){
+          var state = this.sessionState, invoker = this.router.addRoute('get', '/path', this, 'onlyIfLoggedIn')
+          this.router.protectRoute('get', '/path')
+          this.onlyIfLoggedIn = function(req){
+            return Response.ok(req.session)
+          }
+          return expect(invoke(invoker, { 'session-token': this.sessionToken })).to.be.fulfilled.then(function(response){
+            expect(response.status).to.equal(200)
+            expect(response.body).to.shallowDeepEqual(state)
+          })
+        }) 
+      })
     })
 
     describe('#createInvoker', function(){
@@ -29,17 +96,17 @@ describe('REST API Router', function(){
 
       it('throws if no method is provided', function(){
         var router = this.router
-        expect(function(){ router.createInvoker({})}).to.throw
+        expect(function(){ router.createInvoker({})}).to.throw()
       })
 
       it('throws if method is not a string', function(){
         var router = this.router
-        expect(function(){ router.createInvoker({}, 1)}).to.throw
+        expect(function(){ router.createInvoker({}, 1)}).to.throw()
       })
 
       it('throws if object is not an object', function(){
         var router = this.router
-        expect(function(){ router.createInvoker('notAnObject', 'method') }).to.throw
+        expect(function(){ router.createInvoker('notAnObject', 'method') }).to.throw()
       })
 
       describe('invoker function', function(){
@@ -48,12 +115,12 @@ describe('REST API Router', function(){
         })
 
         it('throws if method doesn\'t exists in object', function(){
-          expect(this.invoker).to.throw
+          expect(this.invoker).to.throw()
         })
 
         it('throws if method is not a function', function(){
           this.invokeMethod = 'string'
-          expect(this.invoker).to.throw
+          expect(this.invoker).to.throw()
         })
 
         it('should invoke method when called with this set to object', function(){
@@ -83,39 +150,27 @@ describe('REST API Router', function(){
 
         it('should use default invoked method return values, if missing', function(){
           this.invokeMethod = function(){}
-          this.invoker({},{
-            send: function(status, body){
-              expect(status).to.equal(500)
-              expect(body).to.have.property('code', 'InternalError')
-              expect(body).to.have.property('message', 'invalid service response')
-            }
-          }, function(){})
+          expect(invoke(this.invoker)).to.become(Response.internalError('invalid sevice response'))
         })
 
         it('invoked method can return a promise', function(){
-          var invoked = false, defered = Promise.defer()
+          var res, defered = Promise.defer()
           this.invokeMethod = function(){ return defered.promise }
-          var p = this.invoker({},{ send: function(status, body){ invoked = true } }, function(){})
-          expect(invoked).to.be.false
-          defered.resolve()
+          var p = this.invoker({},{ send: function(s, b){ res = { status: s, body: b } } }, function(){})
+          defered.resolve(Response.ok('abc'))
           return expect(p).to.eventually.be.fulfilled.then(function(){
-            expect(invoked).to.be.true
+            expect(res).to.eql(Response.ok('abc'))
           })
         })
 
-        it('invoked method with a promise can throw an exception and converted to internalError', function(){
-          var status, body, defered = Promise.defer()
+        it('invoked method with a promise can reject and converted to internalError', function(){
+          var res, defered = Promise.defer()
           this.invokeMethod = function(){ return defered.promise }
-          var p = this.invoker({},{ send: function(s, b){ status = s; body = b } }, function(){})
-          defered.reject(new Error('error'))
+          var p = this.invoker({},{ send: function(s, b){ res = { status: s, body: b } } }, function(){})
+          defered.reject('err')
           return expect(p).to.eventually.be.fulfilled.then(function(){
-            expect(status).to.equal(500)
-            expect(body).to.eql({
-              code: 'InternalError'
-            , message: 'error'
-            })
+            expect(res).to.eql(Response.internalError('err'))
           })
-
         })
 
         it('invoked method are passed a request object', function(){
@@ -127,16 +182,10 @@ describe('REST API Router', function(){
         })
 
         it('should transfor exceptions inside invoked method to internalError', function(){
-          var status, body
           this.invokeMethod = function(){
             throw Error('my error')
           }
-          this.invoker({}, { send: function(s, b){ status = s; body = b } }, function(){})
-          expect(status).to.equal(500)
-          expect(body).to.eql({
-            code: 'InternalError'
-          , message: 'my error'
-          })
+          return expect(invoke(this.invoker, {})).to.become(Response.internalError('my error'))
         })
 
       })
@@ -155,7 +204,7 @@ describe('REST API Router', function(){
 
       it('throws if a route method doesn\'t exists in the httpProvider', function(){
         var self = this
-        expect(function(){ self.router.addRoute('unknown', '/path', {}, 'method') }).to.throw
+        expect(function(){ self.router.addRoute('unknown', '/path', {}, 'method') }).to.throw()
       })
     })
 
@@ -187,3 +236,15 @@ describe('REST API Router', function(){
 
 })
                                    
+function invoke(invoker, request){
+  var defer = Promise.defer()
+
+  invoker(request || {}, {
+    send: function(status, body){
+      defer.resolve({ status: status, body: body })
+    }
+  }, function(){})
+
+  return defer.promise
+}
+

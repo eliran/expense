20807712f39649883ca50d3a94e17d0e047cc2b5
Eliran Ben-Ezra <eliran@threeplay.com>
2016-07-06 17:56:47 -0400

    database JSON configuration loader

---
+++ test/unit/dataStoreSpec.js
@@ -0,0 +1,96 @@
+var helpers = require('../helpers')
+  , expect = helpers.expect
+  , DataStore = require('../../lib/dataStore')
+
+describe('Data store', function(){
+
+   
+  it('#readConfigFile should return JSON config', function(){
+     expect(readConfig()).to.have.keys('$common','development', 'production', 'default')
+  })
+
+  function readConfig(){
+    return DataStore.readConfigFile(configFileName())
+  }
+  function configFileName(){
+    return __dirname + '/testConfig.json'
+  }
+
+  it('#parseConfig: returns default config if no env specified', function(){
+    expect(DataStore.parseConfig(readConfig())).to.eql({
+      key: 'default'
+    , common: 'common'
+    })
+  })
+ 
+  it('#parseConfig: returns default config if unknown env specified', function(){
+    expect(DataStore.parseConfig(readConfig()), 'someEnv').to.eql({
+      key: 'default'
+    , common: 'common'
+    })
+  })
+
+  it('#parseConfig: returns selected env', function(){
+    expect(DataStore.parseConfig(readConfig(), 'development')).to.eql({
+      key: 'dev'
+    , common: 'common'
+    })
+  })
+ 
+  it('#praseConfig: returns config values of $keys expend from object param', function(){
+    expect(DataStore.parseConfig(readConfig(), 'production', { NODE_ENV: 'fromEnv' })).to.eql({
+      'var': 'fromEnv'
+    , other: 'NODE_ENV'
+    , common: 'common'
+    })
+  })
+
+  it('#getConfig(string): loads a file with development env', function(){
+    expect(DataStore.getConfig(configFileName())).to.eql({
+      key: 'dev'
+    , common: 'common'
+    })
+  })
+
+  it('#getConfig(string, env): loads a file with env', function(){
+    expect(DataStore.getConfig(configFileName(), 'default')).to.eql({
+      key: 'default'
+    , common: 'common'
+    })
+  })
+
+  it('#getConfig(object, env): processes object as config', function(){
+    expect(DataStore.getConfig({env: { key: 'value' }}, 'env')).to.eql({
+      key: 'value'
+    })
+  })
+
+  it('dataStore can be created with config', function(){
+    var dataStore = new DataStore(configFileName(), 'development')
+    expect(dataStore.config).to.eql({
+      key: 'dev'
+    , common: 'common'
+    })
+  })
+
+  describe('instance', function(){
+    beforeEach(function(){
+      this.dataStore = new DataStore()
+    })
+
+    it('should initially have no store', function(){
+       expect(this.dataStore.store).to.be.null
+    })
+
+    it('should get a store from ready callback', function(done){
+       this.dataStore.ready(function(err, store){
+         expect(store).to.not.be.null
+         done()
+       })
+    })
+
+    it('should get a store from #getStore', function(){
+      expect(this.dataStore.getStore()).to.not.be.null
+    })
+  })
+})
